391a486ccb9c0f8fe2db29820c7a619556f6e710
Cody Young <cyoung1623@gmail.com>
2019-07-08 19:48:37 -0700

    Basic layout complete

--- js/shoes_strings.js
+++ js/shoes_strings.js
@@ -1,7 +1,35 @@
 // Variables
-
+var prices = [{model: "jordan_1", price: 169.99}, 
+             {model: "ultraboost", price: 199.99}]
 
 // Listeners
 
+//Functions
+
+// Calculates the cost of each pair of Jordan 1s.
+function calcJordan {
+
+}
+
+
+// Calculates the cost of each pair of Ultraboosts. 
+function calcBoost {
+
+}
+
+function calcSubTotal {
+
+}
+
+function updateCartTotal {
+
+}
+
 
 // jQuery Handlers
+
+
+// Shows checkout confirmation message.
+
+// Updates subtotal, taxes, and final total of cart.
+
